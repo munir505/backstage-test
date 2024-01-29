dd2ced55ca68d64cba1a31defacbd3dde7530b6b
Munir <31385912+munir505@users.noreply.github.com>
2024-01-29 12:59:11 +0000

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,22 +1,22 @@
-# resource "google_compute_instance" "test-workflow" {
-#   name         = "test-workflow"
-#   zone         = "europe-west2-c"
-#   machine_type = "n1-standard-2"
+resource "google_compute_instance" "test-workflow" {
+  name         = "test-workflow"
+  zone         = "europe-west2-c"
+  machine_type = "n1-standard-2"
 
-#   network_interface {
-#     subnetwork = "projects/coen-munir-kakar/regions/europe-west2/subnetworks/test-subnet"
-#   }
+  network_interface {
+    subnetwork = "projects/coen-munir-kakar/regions/europe-west2/subnetworks/test-subnet"
+  }
 
-#   boot_disk {
-#     auto_delete = "false"
-#     source      = google_compute_disk.workflow-test-disk.self_link
-#   }
-# }
+  boot_disk {
+    auto_delete = "false"
+    source      = google_compute_disk.workflow-test-disk.self_link
+  }
+}
 
-# resource "google_compute_disk" "workflow-test-disk" {
-#   name  = "workflow-test-disk"
-#   type  = "pd-ssd"
-#   zone  = "europe-west2-c"
-#   size  = 100
-#   image = "ubuntu-2004-focal-v20240125"
-# }
+resource "google_compute_disk" "workflow-test-disk" {
+  name  = "workflow-test-disk"
+  type  = "pd-ssd"
+  zone  = "europe-west2-c"
+  size  = 100
+  image = "ubuntu-2004-focal-v20240125"
+}
